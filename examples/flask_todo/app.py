edf7b5f037174ea32dd425c5d2e4248ae8ca2d83
Tony Narlock <tony@git-pull.com>
2018-10-28 13:26:10 -0500

    :truck: Move flask_admin, mongo_profiler wrapper to example

--- examples/flask_todo/app.py
+++ examples/flask_todo/app.py
@@ -24,4 +24,43 @@ def create_app():
     return app
 
 
+def setup_flask_mongo_profiler(app):
+    from flask_mongo_profiler.contrib.werkzeug.mongo import QueuedMongoCommandLogger
+    from flask_mongo_profiler.contrib.werkzeug.werkzeug_middleware import (
+        ProfilerMiddleware
+    )
+    import pymongo
+
+    mongo_logger = QueuedMongoCommandLogger()
+    pymongo.monitoring.register(mongo_logger)
+    app.wsgi_app = ProfilerMiddleware(
+        app.wsgi_app,
+        pymongo_logger=mongo_logger,
+        ignored_url_patterns=['admin', 'favicon.ico'],
+    )
+    return app
+
+
+def setup_flask_admin(app):
+    from flask_admin import Admin
+    from flask_mongo_profiler.contrib.flask_admin.views import (
+        profiling as profiling_views
+    )
+    from flask_mongo_profiler.contrib.mongoengine import profiling as profiling_models
+
+    admin = Admin(name='Peergrade', template_mode='bootstrap3')
+    admin.add_view(
+        profiling_views.ProfilingRequestView(
+            profiling_models.ProfilingRequest, category='Profiling', name='Requests'
+        )
+    )
+    admin.add_view(
+        profiling_views.ProfilingQueryView(
+            profiling_models.ProfilingQuery, category='Profiling', name='Queries'
+        )
+    )
+    admin.init_app(app)
+    return app
+
+
 app = create_app()
