4025070a870696f995b54c610046217fefba3100
Tony Narlock <tony@git-pull.com>
2018-10-28 15:18:35 -0500

    :white_check_mark: Assure tests hit WSGI middleware
    See also:
    - https://github.com/cdent/wsgi-intercept
    - https://github.com/pytest-dev/pytest-flask/issues/74
    - https://github.com/pallets/flask/issues/1844

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 import pytest
 
+import requests
 from examples.flask_todo.app import create_app
 from flask import url_for
+from wsgi_intercept.interceptor import RequestsInterceptor
+
+from flask_mongo_profiler.contrib.mongoengine import profiling as profiling_models
 
 
 @pytest.fixture
@@ -14,8 +18,30 @@ def app():
     return app
 
 
+@pytest.fixture(autouse=True)
+def clear_db():
+    profiling_models.ProfilingRequest.objects.delete()
+    profiling_models.ProfilingQuery.objects.delete()
+
+
 def test_example(app, client):
-    client.get(url_for('index'))
+    with RequestsInterceptor(lambda: app.wsgi_app, host='localhost', port=5000) as url:
+        response = requests.get(url)
+        assert response.status_code == 200
+
+    assert profiling_models.ProfilingRequest.objects.count() == 1
+    assert profiling_models.ProfilingQuery.objects.count() == 1
     client.get(url_for('admin.index'))
+
+    # List of requests
     client.get(url_for('profilingrequest.index_view'))
+
+    # Pull a profiling artifact of the request created when visiting 'index'
+    request_profile = profiling_models.ProfilingRequest.objects.first()
+    client.get(url_for('profilingrequest.details_view', id=request_profile.id))
+
     client.get(url_for('profilingquery.index_view'))
+
+    # Same as above,
+    query_profile = profiling_models.ProfilingQuery.objects.first()
+    client.get(url_for('profilingquery.details_view', id=query_profile.id))
