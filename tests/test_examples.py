8a5988f31067a3cece0c83e2238c42a095bad218
Tony Narlock <tony@git-pull.com>
2018-10-28 13:01:46 -0500

    :white_check_mark: Add profiler to test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,10 +7,31 @@ from examples.flask_todo.app import create_app
 from flask import url_for
 
 
+def setup_profiler(app):
+    from flask_mongo_profiler.contrib.werkzeug.mongo import (
+        LoggingMongoCommandLogger,
+        QueuedMongoCommandLogger,
+    )
+    from flask_mongo_profiler.contrib.werkzeug.werkzeug_middleware import (
+        ProfilerMiddleware
+    )
+    import pymongo
+
+    mongo_logger = QueuedMongoCommandLogger()
+    pymongo.monitoring.register(mongo_logger)
+    app.wsgi_app = ProfilerMiddleware(
+        app.wsgi_app,
+        pymongo_logger=mongo_logger,
+        ignored_url_patterns=['admin', 'favicon.ico'],
+    )
+    return app
+
+
 @pytest.fixture
 def app():
     app = create_app()
     app.debug = True
+    setup_profiler(app)
     return app
 
 
